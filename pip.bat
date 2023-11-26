b49775fcd4914dae4f4073308422780e22ad477e
Wolffighters <45029767+Wolffighters@users.noreply.github.com>
2023-11-26 17:26:40 +1100

    Add files via upload

---
+++ pip.bat
@@ -0,0 +1,13 @@
+winget install --accept-package-agreements --id 9NRWMJP3717K
+py -3.11 -m ensurepip --upgrade
+py -3.11 -m pip install aiohttp
+py -3.11 -m pip install aiosignal
+py -3.11 -m pip install async-timeout
+py -3.11 -m pip install async-mcrcon
+py -3.11 -m pip install asyncio
+py -3.11 -m pip install discord.py
+py -3.11 -m pip install discord-py-interactions
+py -3.11 -m pip install discord-typing
+py -3.11 -m pip install disnake
+py -3.11 -m pip install termcolor
+py -3.11 -m pip install rcon
